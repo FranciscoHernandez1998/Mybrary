ac545ad0577c57617f0b466ff1000801e1f93249
Francisco <fjch87@gmail.com>
2020-06-21 01:47:33 -0600

    Authors routes init

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
-const indexRouter = require('./routes/index.js')
+const bodyParser=require('body-parser');
 
+
+const indexRouter = require('./routes/index.js');
+const authorRouter = require('./routes/authors.js');
 
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
 app.set('layout',path.join(__dirname,'views','layouts','layout'));
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}));
 
 const mongoose = require('mongoose');
 
@@ -22,6 +26,7 @@ db.on('error', error=> console.error(error));
 db.once('open', ()=>console.log('Connected to mongoose'));
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 
 
 const PORT = process.env.PORT || 5000;
